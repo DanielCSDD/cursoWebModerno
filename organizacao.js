8a11b2297516824cc120413dd0aff85ef20310b9
Daniel Coelho <daniel.coelho.santos@gmail.com>
2022-12-14 14:44:22 -0300

    Atualizando o arquivo: organizacao.js (Referente a aula 35).

---
+++ organizacao.js
@@ -0,0 +1,12 @@
+// Um arquivo Javascript pode ser constituído por: sentenças de códigos e/ou blocos de códigos. 
+
+// Representação de uma sentença de código.
+console.log('Representação de uma sentença de código')
+
+// Representação de um bloco de código.
+{ // Um bloco pode conter vários blocos internamente.
+    { // Declarando um bloco interno.
+        console.log('Olá,')
+        console.log('Mundo')
+    }
+}
