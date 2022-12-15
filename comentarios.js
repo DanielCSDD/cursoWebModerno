a720488d63377dc5e475e59c6d88b6cf9e1f4fab
Daniel Coelho <daniel.coelho.santos@gmail.com>
2022-12-15 08:11:55 -0300

    Criando arquivo: comentario.js

---
+++ comentarios.js
@@ -0,0 +1,12 @@
+// Comentário de uma única linha.
+console.log('Comentário de uma única linha!')
+
+/**
+ * Comentário de múltiplas linhas.
+ */
+console.log('Cometário de múltiplas linhas, exemplo 01!')
+
+/*
+ * Commentário de múltiplas linhas.
+ */
+console.log('Cometário de múltiplas linhas, exemplo 02!')
