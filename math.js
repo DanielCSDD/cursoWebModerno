a03f1a990696124b8c8f37b197217de80534ca9b
Daniel Coelho <daniel.coelho.santos@gmail.com>
2022-12-21 08:29:30 -0300

    Criando arquivo: math.js

---
+++ math.js
@@ -0,0 +1,15 @@
+
+// É um objeto da API de Javascript, que tem muitas utilidades.
+const raio = 5.6
+
+// Math.pow(valor do raio, valor do expoente)
+const area = Math.PI * Math.pow(raio, 2)
+
+// Imprimindo o valor de area
+console.log(area)
+
+// Corrigindo as casas decimais
+console.log(area.toFixed(2))
+
+// Verificando o tipo da variável area.
+console.log(typeof area)
